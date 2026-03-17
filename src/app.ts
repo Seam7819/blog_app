3997a74e7b0b81177547d3ca776918115472bd95
Snu Seam <siamahme766@gmail.com>
2026-03-17 12:26:56 +0600

    btauth

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express"
 import { postRoute } from "./module/post/post.route";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = express();
-
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.use(express.json());
 
 app.get('/', (req:Request,res:Response)=>{
